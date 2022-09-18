c63d528cf63b2982848506a2e29991310e03f8fa
José Galarza <113070189+JoseGalarza85@users.noreply.github.com>
2022-09-18 16:32:08 -0300

    Add files via upload

---
+++ Clase 9/Clase 9b/script.js
@@ -0,0 +1,10 @@
+var pais = prompt("Ingrese un pais: ");
+var continente = prompt("Ingrese el continente: ");
+var antiguedad = prompt("Antigüedad: ");
+var pais_y_continente = pais + " de " + continente;
+
+pais = "Mexico";
+continente = "America";
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
